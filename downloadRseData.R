1ab9cd82443c59a908339f94838953855273e754
AnVaBioInf <96749066+AnVaBioInf@users.noreply.github.com>
2024-05-05 18:23:41 +0300

    Add files via upload

--- downloadRseData.R
+++ downloadRseData.R
@@ -1,85 +1,80 @@
-library(recount3)
+### In this file project rse data are downloaded, filtered by genes (only cytoskeleton genes are left)
+### and gene names are added to rse@rowRanges 
+
+# library(recount3)
 # 
-# # load counts ######
+# # -- loading project rse
 # human_projects <- recount3::available_projects()
 # proj_info <- subset(
 #   human_projects,
 #   project == "ERP109002" & project_type == "data_sources"
 # )
-# 
-# rse.ERP109002 = create_rse(proj_info)  # genes
-# rse.ERP109002.jxn <- create_rse(proj_info,
+# rse.gene = create_rse(proj_info) 
+# rse.jxn <- create_rse(proj_info,
 #                                 type="jxn") # junctions
 # 
-# # write R object to a file
-# saveRDS(rse.ERP109002,'rse.ERP109002.rds')
-# saveRDS(rse.ERP109002.jxn,'rse.ERP109002.jxn.rds')
+# # -- gene rse only with cytoskeleton genes --
+# # cytoskeleton genes
+# cytoskeleton.genes = rbind(data.frame(gene_name=c('CYFIP1','CYFIP2','NCKAP1','NCKAP1L','ABI1','ABI2','ABI3','WASF1','WASF2','WASF3','BRK1'),group='WAVE'),
+#                            data.frame(gene_name=c('NHS','NHSL1','NHSL2','KIAA1522'),group='NHS'),
+#                            data.frame(gene_name=c('ARPC1A','ARPC1B','ARPC2','ARPC3','ARPC4','ARPC5','ACTR2','ACTR3','ACTR3B'),group='Arp2/3'))
+# # 24 genes
 # 
-# genes = rbind(data.frame(gene_name=c('CYFIP1','CYFIP2','NCKAP1','NCKAP1L','ABI1','ABI2','ABI3','WASF1','WASF2','WASF3','BRK1'),group='WAVE'),
-#               data.frame(gene_name=c('NHS','NHSL1','NHSL2','KIAA1522'),group='NHS'),
-#               data.frame(gene_name=c('ARPC1A','ARPC1B','ARPC2','ARPC3','ARPC4','ARPC5','ACTR2','ACTR3','ACTR3B'),group='Arp2/3'))
-# 
-# # leaving only citoskeleton genes in rse.ERP109002
-# genes.annotaion = as.data.frame(rse.ERP109002@rowRanges[rse.ERP109002@rowRanges$gene_name %in% genes$gene_name,])
+# rse.gene.cytosk = rse.gene[rse.gene@rowRanges$gene_name %in% cytoskeleton.genes$gene_name,]
 # 
 # # adding group names
-# genes.annotaion$group = genes$group[match(genes.annotaion$gene_name,genes$gene_name)]
+# rse.gene.cytosk@rowRanges$group = cytoskeleton.genes$group[
+#   match(rse.gene.cytosk@rowRanges$gene_name,cytoskeleton.genes$gene_name)]
 # 
 # # leaving only genes on main chr
-# genes.annotaion = genes.annotaion[startsWith(as.character(genes.annotaion$seqnames),'chr'),]
+# rse.gene.cytosk@rowRanges = 
+#   rse.gene.cytosk@rowRanges[startsWith(as.character(rse.gene.cytosk@rowRanges@seqnames),'chr'),]
 # 
 # # sorting
-# genes.annotaion = genes.annotaion[order(genes.annotaion$seqnames, genes.annotaion$start), ]
+# rse.gene.cytosk@rowRanges = 
+#   rse.gene.cytosk@rowRanges[order(rse.gene.cytosk@rowRanges@seqnames, rse.gene.cytosk@rowRanges@ranges), ]
 # 
-# saveRDS(genes.annotaion,'genes.annotaion.rds')
+# # subsetting seqinfo 
+# new_seqinfo <- Seqinfo(seqnames = as.character(unique(rse.gene.cytosk@rowRanges@seqnames)),
+#                        seqlengths = NA,
+#                        isCircular = NA)
+# seqlevels(rse.gene.cytosk@rowRanges) <- seqlevels(new_seqinfo)
 # 
-# # -----
+# # Assign the new Seqinfo to the GRanges object
+# seqinfo(rse.gene.cytosk@rowRanges) <- new_seqinfo
 # 
-rse.ERP109002 = readRDS('rse.ERP109002.rds', refhook = NULL)
-rse.ERP109002.jxn = readRDS('rse.ERP109002.jxn.rds', refhook = NULL)
-genes.annotaion = readRDS('genes.annotaion.rds', refhook = NULL)
-# 
-# #-----------------------------------------
-# 
-# # --junxtion "annotation"
+# # --making junxtion "annotation"
 # # intersecting ranges
-# overlaps = findOverlaps(query = rse.ERP109002.jxn@rowRanges, 
-#                         subject = rse.ERP109002@rowRanges[genes.annotaion$gene_id], 
-#                         type='any') # or within?
+# # selecting only cytoskeleton genes in rse.jxn and adding gene column to it (making jxn "annotation")
+# # The function compares each interval in the query object with the intervals in the subject object and identifies any overlaps.
+# overlaps = findOverlaps(query = rse.jxn@rowRanges,
+#                         subject = rse.gene.cytosk@rowRanges,
+#                         type='within') # the start and end positions of the query range must fall within the start and end positions of the subject range.
 # 
-# #rse.ERP109002.jxn@rowRanges[64106]
-# #rse.ERP109002@rowRanges[genes_annotaion$gene_id][1]
-# # junction is larger than gene?
+# # check
+# rse.jxn@rowRanges[64374]
+# rse.gene.cytosk@rowRanges[1]
 # 
-# rse.ERP109002.jxn.cytosk.genes = rse.ERP109002.jxn[overlaps@from,]  
-# rse.ERP109002.jxn.cytosk.genes@rowRanges$gene_id = genes.annotaion$gene_id[overlaps@to]
-# rse.ERP109002.jxn.cytosk.genes@rowRanges$gene_names = genes.annotaion$gene_name[overlaps@to]
+# # selecting only jxns of cytoskeleton genes and assing gene info
+# rse.jxn.cytosk = rse.jxn[overlaps@from,]
+# rse.jxn.cytosk@rowRanges$gene_id = rse.gene.cytosk@rowRanges$gene_id[overlaps@to]
+# rse.jxn.cytosk@rowRanges$gene_names = rse.gene.cytosk$gene_name[overlaps@to]
 # 
-# #?????? перенести этот код до оверлепинга!? Что за ворнинг?
 # # -- removing duplicates
-# counts = as.matrix(rse.ERP109002.jxn.cytosk.genes@assays@data$counts) 
-# # duplicated junctions
-# rownames(counts)[duplicated(rownames(counts))]
-# table(duplicated(rownames(counts)))
-# # adding column with junction ids (to make sure that it finds duplicates only for the same junctions)
-# counts = cbind(ids=rownames(counts), counts)
-# # dropping duplicated rows
-# rse.ERP109002.jxn.cytosk.genes = rse.ERP109002.jxn.cytosk.genes[which(!duplicated(counts)),]
+# # checking
+# table(duplicated(rse.jxn.cytosk@rowRanges@ranges@NAMES))
+# nms = unique(rse.jxn.cytosk@rowRanges@ranges@NAMES)
+# rse.jxn.cytosk = rse.jxn.cytosk[nms,]
 # 
+# # removing unneeded data
+# rm(overlaps, rse.gene, rse.jxn)
 # 
-# 
-# rse.ERP109002.jxn.cytosk.genes = saveRDS(rse.ERP109002.jxn.cytosk.genes,'rse.ERP109002.jxn.cytosk.genes.rds')
-rse.ERP109002.jxn.cytosk.genes = readRDS('rse.ERP109002.jxn.cytosk.genes.rds', refhook = NULL)
+# # -- saving files
+# saveRDS(rse.gene.cytosk,'rse.gene.cytosk.rds')
+# saveRDS(rse.jxn.cytosk,'rse.jxn.cytosk.rds')
 
-# если что то сломается- искать здесь!!!!
-#rm(overlaps, counts, rse.ERP109002, rse.ERP109002.jxn, genes.annotaion)
-
-# checking
-#counts = as.matrix(rse.ERP109002.jxn.cytosk.genes@assays@data$counts) 
-#table(duplicated(rownames(counts)))
+# -- reading files
+rse.gene.cytosk = readRDS('rse.gene.cytosk.rds', refhook = NULL)
+rse.jxn.cytosk = readRDS('rse.jxn.cytosk.rds', refhook = NULL)
 
 
-# preprocessing annotation
-#source('samples_ann_preprocessing.R')
-
-# почему не сохранить все в rse.ERP109002.jxn.cytosk.genes.rse?
