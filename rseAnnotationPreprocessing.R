7c3f7acfcc67f205f11c587dcde4a8b90bbce4e1
AnVaBioInf <96749066+AnVaBioInf@users.noreply.github.com>
2024-05-05 22:19:45 +0300

    Add files via upload

--- rseAnnotationPreprocessing.R
+++ rseAnnotationPreprocessing.R
@@ -1,88 +1,104 @@
-source("data.R")
-# rse.ERP109002.jxn.cytosk.genes = readRDS('rse.ERP109002.jxn.cytosk.genes.rds', refhook = NULL)
-# 
-# # metadata
-# # renaming to shorten the name
-# rse = rse.ERP109002.jxn.cytosk.genes
-# 
-# # from sample metadata extracting sra.library_name, containing information on tissue, age etc
-# split_cols = strsplit(rse@colData$sra.library_name, "\\.")
-# rse@colData = within(rse@colData, {
-#   sample_id <- sapply(split_cols, "[[", 1)
-#   organism <- sapply(split_cols, "[[", 2)
-#   tissue <- sapply(split_cols, "[[", 3)
-#   age <- sapply(split_cols, "[[", 4)
-#   gender <- sapply(split_cols, "[[", 5)
-# })
-# 
-# # combining some tissues
-# rse@colData$tissue = sub("Forebrain", "Brain", rse@colData$tissue)
-# rse@colData$tissue = sub("Hindbrain", "Cerebellum", rse@colData$tissue)
-# rse@colData$tissue = sub("KidneyTestis", "Kidney", rse@colData$tissue)
-# unique(rse@colData$tissue)
-# # [1] "Brain"      "Heart"      "Cerebellum" "Kidney"     "Liver"      "Ovary"      "Testis"    
-# 
-# # age measures
-# #unique(gsub("[^[:alpha:]]", "", rse@colData$age))
-# 
-# # order of columns (age)
-# measures_order <- c("CS", "w", "dpb", "mpb", "ypb")
-# unique_ages = unique(rse@colData$age)
-# ages_order = c()
-# for (i in c("CS", "w", "dpb", "mpb", "ypb")){
-#   time_group_values = grep(paste0('^', i, '|', i, '$'), unique_ages, value=TRUE)
-#   period_numbers = as.numeric(gsub("\\D", "", time_group_values))
-#   ages_order = c(ages_order, 
-#                  time_group_values[order(period_numbers)])
-# }
-# #print(paste0("'",ages_order, "'", collapse = ", "))
-# #length(ages_order) == length(unique_ages)
-# 
-# # merging age groups
-# # CS - Carnegie Stage 
-# fetus = c('CS13', 'CS14', 'CS16', 'CS17', 'CS18', 'CS19', 'CS20', 'CS21', 'CS22', 'CS23', '4w', '5w', '6w', '7w', '8w', '9w', '10w', '11w', '12w', '13w', '16w', '18w', '19w', '20w')
-# infant = c('0dpb', '4dpb', '6dpb', '15dpb', '18dpb', '19dpb', '34dpb', '94dpb', '127dpb', '221dpb', '226dpb', '270dpb', '271dpb', '6mpb', '1ypb') # infant - child before the age of 12 months
-# toddler = c('2ypb') # child between 2 and 3 years
-# school = c('4ypb', '7ypb','8ypb')
-# teen = c("13ypb","14ypb","16ypb","17ypb" ) # 13-19 years
-# yo_25_35 = c("25ypb","28ypb","29ypb","32ypb" )
-# yo_36_45 = c("39ypb" )
-# yo_46_55 = c("46ypb", "50ypb", "53ypb" , "54ypb", "55ypb")
-# yo_56_65 = c("58ypb")
-# all_age_gr = c(infant, toddler, school, teen, yo_25_35, yo_36_45, yo_46_55, yo_56_65)
-# 
-# # -- Adding column age group
-# rse@colData$age_group[
-#   rse@colData$tissue != 'Heart' &
-#   rse@colData$age %in% fetus] = "fetus"
-# 
-# rse@colData$age_group[
-#     rse@colData$tissue == 'Heart' &
-#     rse@colData$age %in% c('CS13', 'CS14', 'CS16','4w', '5w', '6w', '7w', '8w', '9w', '10w')] = "fetus"
-# 
-# rse@colData$age_group[
-#   !(rse@colData$tissue %in% c('Testies', 'Kidney')) &
-#   rse@colData$age %in% c(toddler, school, teen, yo_25_35, yo_36_45, yo_46_55, yo_56_65)] = "adult"
-# 
-# # including infant into adult group for kidney, since otherwise there is not enoug of samples 
-# rse@colData$age_group[
-#   rse@colData$tissue == 'Kidney' &
-#   rse@colData$age %in% c(infant, toddler, school, teen, yo_25_35, yo_36_45, yo_46_55, yo_56_65)] = "adult"
-# 
-# # excluding teen from Testies
-# rse@colData$age_group[
-#   rse@colData$tissue == 'Testis' &
-#   rse@colData$age %in% c(yo_25_35, yo_36_45, yo_46_55, yo_56_65)] = "adult"
-# 
-# # removing ovary samples, since there are not enough samples for statistical analysis
-# rse = rse[,rse@colData$tissue != 'Ovary']
-# rse = rse[,rse@colData$age_group %in% c('fetus','adult')]
-# 
-# # renaming object back
-# rse.ERP109002.jxn.cytosk.genes = rse
-# 
-# rm(rse, split_cols)
-# 
-# saveRDS(rse.ERP109002.jxn.cytosk.genes,'rse.ERP109002.jxn.cytosk.genes.rds')
-rse.ERP109002.jxn.cytosk.genes = readRDS('rse.ERP109002.jxn.cytosk.genes.rds', refhook = NULL)
-#rse@assays@data$counts) 
+source("downloadRseData.R")
+# metadata
+# from sample metadata extracting sra.library_name, containing information on tissue, age etc
+split.sra.library_name = strsplit(rse.jxn.cytosk@colData$sra.library_name, "\\.")
+# create a matrix to store the extracted values
+ncols = lengths(split.sra.library_name)[1]
+matrix.sra.library_name = matrix(nrow = nrow(rse.jxn.cytosk@colData), ncol = ncols)
+colnames(matrix.sra.library_name) = c("sample_id", "organism", "tissue", "age", "gender")
+
+for (i in 1:nrow(matrix.sra.library_name)) {
+  matrix.sra.library_name[i, ] = split.sra.library_name[[i]]
+}
+# add the extracted columns to the colData
+rse.jxn.cytosk@colData <- cbind(rse.jxn.cytosk@colData, matrix.sra.library_name)
+rm(matrix.sra.library_name, split.sra.library_name, ncols)
+
+# ---
+# combining some tissues
+rse.jxn.cytosk@colData$tissue = sub("Forebrain", "Brain", rse.jxn.cytosk@colData$tissue)
+rse.jxn.cytosk@colData$tissue = sub("Hindbrain", "Cerebellum", rse.jxn.cytosk@colData$tissue)
+rse.jxn.cytosk@colData$tissue = sub("KidneyTestis", "Kidney", rse.jxn.cytosk@colData$tissue)
+unique(rse.jxn.cytosk@colData$tissue)
+# [1] "Brain"      "Heart"      "Cerebellum" "Kidney"     "Liver"      "Ovary"      "Testis"
+
+
+# ---
+# -- CS - Carnegie Stage to weeks
+cs.to.days = c('CS13' = 32, 'CS14' = 33, 'CS15' = 36, 'CS16' = 39, 'CS17' = 41, 'CS18' = 44, 
+               'CS19' = 46, 'CS20' = 49, 'CS21' = 51, 'CS22' = 53, 'CS23' = 56) # dpc
+# CS to weeks
+cs.to.w = round(cs.to.days/4)
+cs.to.w = paste(cs.to.w, "w", sep="") # adding w, that's how all values are stored in age column age+measure
+names(cs.to.w) = names(cs.to.days) # asigning CS stages in correspondance to each week
+
+# replacing CS with w
+cs.idx = which(rse.jxn.cytosk@colData$age %in% names(cs.to.w)) # index of cs values to replace
+cs.values = rse.jxn.cytosk@colData$age[cs.idx] # selecting all cs values
+rse.jxn.cytosk@colData$age = replace(rse.jxn.cytosk@colData$age, cs.idx, cs.to.w[cs.values]) 
+
+# separating age and measure into two columns
+# replace w (weeks) with wpc (weeks post conseption) (w is wpc according to figure 1 in doi: 10.1038/s41586-019-1338-5)
+rse.jxn.cytosk@colData$age = gsub('w$', 'wpc', rse.jxn.cytosk@colData$age)
+rm(cs.idx, cs.values, cs.to.w, cs.to.days)
+
+# --
+# getting ordered list of ages
+measures_order <- c("wpc", "dpb", "mpb", "ypb")
+unique_ages = unique(rse.jxn.cytosk@colData$age)
+ages_order = c()
+for (i in c("wpc", "dpb", "mpb", "ypb")){
+  time_group_values = grep(paste0('^', i, '|', i, '$'), unique_ages, value=TRUE)
+  period_numbers = as.numeric(gsub("\\D", "", time_group_values))
+  ages_order = c(ages_order,
+                 time_group_values[order(period_numbers)])
+}
+ages_order
+rm(i, unique_ages)
+
+# merging age groups
+fetus = c("4wpc", "5wpc", "6wpc", "7wpc", "8wpc", "9wpc",  "10wpc", "11wpc", "12wpc", "13wpc",
+          "14wpc", "16wpc", "18wpc", "19wpc", "20wpc")
+newborn = c('0dpb', '4dpb', '6dpb', '15dpb', '18dpb', '19dpb', '34dpb') # typically refers to a baby from birth up to about 2 months of age
+infant = c('94dpb', '127dpb', '221dpb', '226dpb', '270dpb', '271dpb', '6mpb', '1ypb') # infant - child before the age of 12 months
+toddler = c('2ypb') # child between 2 and 3 years
+school = c('4ypb', '7ypb','8ypb')
+teen = c("13ypb","14ypb","16ypb","17ypb" ) # 13-19 years
+legal.age = c("25ypb", "28ypb", "29ypb", "32ypb", "39ypb",  "46ypb",  "50ypb",  "53ypb", 
+          "54ypb", "55ypb",  "58ypb")
+adult = c(toddler, school, teen, legal.age)
+
+# -- Adding column age group
+# removing ovary samples, since there are not enough samples for statistical analysis
+rse.jxn.cytosk = rse.jxn.cytosk[,rse.jxn.cytosk@colData$tissue != 'Ovary']
+
+# fetus
+# heart develops earlier than other organs, so period before birth was limited to first 10 weeks
+rse.jxn.cytosk@colData$age_group[rse.jxn.cytosk@colData$tissue != 'Heart' &
+                                 rse.jxn.cytosk@colData$age %in% fetus] = "fetus"
+
+rse.jxn.cytosk@colData$age_group[rse.jxn.cytosk@colData$tissue == 'Heart' &
+                                rse.jxn.cytosk@colData$age %in% 
+      c("4wpc", "5wpc",  "6wpc" , "7wpc",  "8wpc",  "9wpc",  "10wpc")] = "fetus"
+
+# adult
+rse.jxn.cytosk@colData$age_group[!(rse.jxn.cytosk@colData$tissue %in% c('Testies', 'Kidney')) &
+                                 rse.jxn.cytosk@colData$age %in% adult] = "adult"
+
+# including infant in adult group for kidney, since otherwise there are not enough of samples
+rse.jxn.cytosk@colData$age_group[rse.jxn.cytosk@colData$tissue == 'Kidney' &
+    rse.jxn.cytosk@colData$age %in% c(infant, adult)] = "adult"
+
+# excluding teen from Testis, considering developmental peculiarities of testis development
+rse.jxn.cytosk@colData$age_group[rse.jxn.cytosk@colData$tissue == 'Testis' &
+                                 rse.jxn.cytosk@colData$age %in% legal.age] = "adult"
+
+rse.jxn.cytosk@colData[100,c('tissue', 'age', "age_group")]
+# leaving only fetus and adult age groups
+rse.jxn.cytosk = rse.jxn.cytosk[,rse.jxn.cytosk@colData$age_group %in% c('fetus','adult')]
+
+# save rse file with formatted annotation
+saveRDS(rse.jxn.cytosk,'rse.jxn.cytosk.rds')
+
+# read rse file
+rse.jxn.cytosk = readRDS('rse.jxn.cytosk.rds', refhook = NULL)
